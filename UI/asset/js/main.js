9257b981e6567a30fc868c06ec954829252b4a8d
Apelehin Okikiola <okikiola.apelehin@gmail.com>
2019-03-28 07:13:06 +0100

    [Feature-#164724740] Added all the UI feature

--- UI/asset/js/main.js
+++ UI/asset/js/main.js
@@ -2,12 +2,19 @@ const navTrigger = document.querySelector(".nav-trigger");
 const sideNav = document.querySelector(".side-nav");
 const sideMenu = document.querySelector(".sidebar-menu"); //admin-dashboard
 
+const sideMenuStff = document.querySelector(".sidebar-menu-st"); //staff dashboard
+const navTriggerBtn = document.querySelector(".nav-trigger");
+
 navTrigger.addEventListener("click", () => {
   sideNav.classList.toggle("visible");
 });
 
 navTrigger.addEventListener("click", () => {
   sideMenu.classList.toggle("visible");
+});
+
+navTriggerBtn.addEventListener("click", () => {
+  sideMenuStff.classList.toggle("visible");
 });
 
 // const next = document.querySelector('.next');
