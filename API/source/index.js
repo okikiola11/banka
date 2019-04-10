eb78049c5f7cf3c16bd8fbd7dd4216c7ec5d4143
Apelehin Okikiola <okikiola.apelehin@gmail.com>
2019-04-10 15:40:58 +0100

    [ft-#165004411] Added express configuration

--- API/source/index.js
+++ API/source/index.js
@@ -1,12 +1,26 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 
 const app = express();
 
-
+app.use(bodyParser.urlencoded({
+    extended: false,
+}));
+app.use(bodyParser.json());
 app.get('/', (req, res) => res.send('Welcome, to the Web Banking Application!'));
+
+app.all('*', (req, res) => {
+    res.status(404).json({
+        status: 404,
+        message: 'The endpoint you have requested does not exist on this server',
+    });
+});
 
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
+    // eslint-disable-next-line no-console
     console.log(` Server is running on PORT: ${port}`);
 });
+
+export default app;
