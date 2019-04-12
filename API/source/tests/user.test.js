ccc9064a4245b6a22efefe0e999f0f8ab5daa4f1
Apelehin Okikiola <okikiola.apelehin@gmail.com>
2019-04-12 09:35:14 +0100

    [ch-#165306809] Added the testing for the user signup endpoint

--- API/source/tests/user.test.js
+++ API/source/tests/user.test.js
@@ -8,6 +8,8 @@ const {
     expect,
 } = chai;
 
+const API_PREFIX = '/api/v1';
+
 describe('Test case for the default for the banka route /', () => {
     describe('/ testing for response', () => {
         it('should return a welcome message to the user', (done) => {
@@ -36,6 +38,59 @@ describe('Test case for the default for the banka route /', () => {
                             message: 'The endpoint you have requested does not exist on this server',
                         })
                         .to.have.all.keys('status', 'message');
+                })
+                .end(done);
+        });
+    });
+});
+
+describe('/ User Auth Signup Endpoint ', () => {
+    describe('/ user signup - User SignUp Validation Test(Required)', () => {
+        it('user signup validation check', (done) => {
+            request(app)
+                .post(`${API_PREFIX}/auth/signup`)
+                .set('Accept', 'application/json')
+                .send({
+                    firstName: '',
+                    lastName: '',
+                    email: '',
+                    phone: '',
+                    gender: '',
+                    password: '',
+                })
+                .expect(401)
+                .expect((response) => {
+                    expect(response.body.status)
+                        .to.equal(401);
+                    expect(response.body.error)
+                        .to.equal('Validation failed, check errors property for more details');
+                    expect(response.body)
+                        .to.have.all.keys('status', 'error', 'errors');
+                })
+                .end(done);
+        });
+
+        it('should allow a user to signup ', (done) => {
+            request(app)
+                .post(`${API_PREFIX}/auth/signup`)
+                .set('Accept', 'application/json')
+                .send({
+                    firstName: 'Okikiola',
+                    lastName: 'Apelehin',
+                    email: 'user@gmail.com',
+                    phone: '08023182819',
+                    gender: 'female',
+                    user: true,
+                    password: 'okiki123',
+                })
+                .expect(201)
+                .expect((response) => {
+                    expect(response.body.status)
+                        .to.equal(201);
+                    expect(response.body.message)
+                        .to.equal('New User has been created');
+                    expect(response.body.data[0])
+                        .to.have.all.keys('auth', 'token', 'payLoad');
                 })
                 .end(done);
         });
