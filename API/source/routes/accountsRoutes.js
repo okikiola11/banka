7d55746079d9eed4ed9803ac52e4f0bc83de372d
Apelehin Okikiola <okikiola.apelehin@gmail.com>
2019-04-14 19:55:16 +0100

    [bg-#165297058] Resolved conflict on ft-credit-account

--- API/source/routes/accountsRoutes.js
+++ API/source/routes/accountsRoutes.js
@@ -11,5 +11,8 @@ const router = Router();
 router.use(authMiddleware.verifyToken);
 router.post('/', authorize.createAccountAuth, Validation.validateAccount(), accountsController.createAccount);
 router.patch('/:accountNumber', authorize.viewAccountAuth, accountsController.updateAccount);
+router.get('/', authorize.viewAccountAuth, accountsController.getAllAccount);
+router.get('/:accountNumber', authorize.viewAccountAuth, accountsController.getSingleAccount);
+router.delete('/:accountNumber', authorize.viewAccountAuth, accountsController.deleteAccount);
 
 export default router;
